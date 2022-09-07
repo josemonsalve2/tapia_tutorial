eccf8f3392b4303c13de3a49363dc961e27250b5
Jose M Monsalve Diaz <jmonsalvediaz@anl.gov>
2022-09-07 01:25:48 -0400

    Single Kernel TAPIA

--- config/TAPIA_MPI/kernel-helper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-module purge
-module load cgpu
-module load openmpi
-module load python
-exec "$@"
--- config/tapia_setup.sh
+++ config/tapia_setup.sh
@@ -4,5 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR
 mkdir -p ~/.local/share/jupyter/kernels/
-cp -r TAPIA_OPENMP ~/.local/share/jupyter/kernels/
-cp -r TAPIA_MPI ~/.local/share/jupyter/kernels/
+cp -r TAPIA ~/.local/share/jupyter/kernels/
